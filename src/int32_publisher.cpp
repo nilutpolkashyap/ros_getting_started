269364fdfbbcf8baba95591e1f631362a7f76151
Nilutpol Kashyap <47447024+nilutpolkashyap@users.noreply.github.com>
2021-01-19 23:37:55 +0530

    uploaded src files

---
+++ src/int32_publisher.cpp
@@ -0,0 +1,28 @@
+#include "ros/ros.h"
+#include "std_msgs/Int32.h"
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "int32_publisher");
+	ros::NodeHandle nh;
+
+	ros::Publisher pub = nh.advertise<std_msgs::Int32>("/integer",10);
+	ros::Rate loop_rate(2);
+
+	int count = 0;
+
+	while(ros::ok())
+	{
+		std_msgs::Int32 msg;
+		msg.data = count;
+		ROS_INFO("I sent: %d",msg.data);
+		pub.publish(msg);
+
+		ros::spinOnce();
+		loop_rate.sleep();
+		++count;
+	}
+	return 0;
+}
