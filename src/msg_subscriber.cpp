135a0b1072b031d2f8f63153496a0324ae4f6f58
Nilutpol Kashyap <47447024+nilutpolkashyap@users.noreply.github.com>
2021-01-20 03:05:19 +0530

    uploaded custom message nodes

---
+++ src/msg_subscriber.cpp
@@ -0,0 +1,20 @@
+#include "ros/ros.h"
+#include "std_msgs/Int32.h"
+#include "ros_basics/greet.h"
+#include <iostream>
+
+void callback(const ros_basics::greet::ConstPtr& msg)
+{
+	ROS_INFO("Received: %s", msg->greeting.c_str());
+	ROS_INFO("Number: %d", msg->number);
+}
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "msg_subscriber");
+	ros::NodeHandle nh;
+	ros::Subscriber sub = nh.subscribe("/greet",10,callback);
+
+	ros::spin();
+	return 0;
+}
