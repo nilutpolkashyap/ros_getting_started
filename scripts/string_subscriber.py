14b130ab72baa2464856137ad98ecf75a0d0c3ac
Nilutpol Kashyap <47447024+nilutpolkashyap@users.noreply.github.com>
2021-01-28 00:25:14 +0530

    Update string_subscriber.py

--- scripts/string_subscriber.py
+++ scripts/string_subscriber.py
@@ -3,13 +3,14 @@ import rospy
 from std_msgs.msg import String
 
 def callback(data):
-	rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+	#rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+	rospy.loginfo("I heard %s", data.data)
 
-def listener():
+def subscriber():
 	rospy.init_node("string_subscriber", anonymous=True)
 	rospy.Subscriber("string", String, callback)
 
 	rospy.spin()
 
 if __name__ == '__main__':
-	listener()
+	subscriber()
