f3b6c1322f914d3e7cf1d41f71869630ebc335ef
Nilutpol Kashyap <47447024+nilutpolkashyap@users.noreply.github.com>
2021-01-27 23:28:01 +0530

    Create string_publisher.py

---
+++ scripts/string_publisher.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import rospy 
+from std_msgs.msg import String
+
+def publisher():
+	pub = rospy.Publisher("string", String, queue_size = 10)
+	rospy.init_node("string_publisher", anonymous=True)
+	rate = rospy.Rate(10)
+
+	while not rospy.is_shutdown():
+		str = "hello world %s" % rospy.get_time()
+		rospy.loginfo(str)
+		pub.publish(str)
+		rate.sleep()
+
+if __name__ == '__main__':
+	try:
+		publisher()
+	except rospy.ROSInterruptException:
+		pass
