0ebaf8aedd6169254c4fa2e568f4ad30045c2adc
Nilutpol Kashyap <47447024+nilutpolkashyap@users.noreply.github.com>
2021-01-28 00:24:51 +0530

    Update int32_publisher.py

--- scripts/int32_publisher.py
+++ scripts/int32_publisher.py
@@ -5,17 +5,17 @@ import random
 
 def publisher():
 	pub = rospy.Publisher('integer',Int32, queue_size  =100)
-	ros.init_node("int32_publisher", anonymous=True)
+	rospy.init_node("int32_publisher", anonymous=True)
 	rate = rospy.Rate(10)
 
 	while not rospy.is_shutdown():
 		var = random.randint(0,100)
 		rospy.loginfo("I sent: %d", var)
 		pub.publish(var)
-		rte.sleep()
+		rate.sleep()
 
 if __name__ == '__main__':
 	try:
 		publisher()
 	except rospy.ROSInterruptException():
-		subc
+		pass
