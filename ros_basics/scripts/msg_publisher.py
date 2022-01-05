21c4e7268718794815c58138351213c0846a7401
Nilutpol Kashyap <nkelectronicshlnp@gmail.com>
2022-01-06 04:10:03 +0530

    custom message publisher code in python

--- ros_basics/scripts/msg_publisher.py
+++ ros_basics/scripts/msg_publisher.py
@@ -5,8 +5,24 @@ from ros_basics.msg import greet
 
 def publisher():
 	rospy.init_node("msg_publisher", anonymous=True)
-	pub = rospy.Publisher('greet', greet, queue_size=100)
+	pub = rospy.Publisher('greeting', greet, queue_size=100)
 
 	rate = rospy.Rate(10)
+	var = 0
 
 	while not rospy.is_shutdown():
+		msg = greet()
+		strg = "Hi there %s" % var
+		msg.greeting = strg
+		rospy.loginfo("Greetings sent : %s", strg)
+		rospy.loginfo("Number sent : %d", var)
+		msg.number = var
+		var = var + 1
+		pub.publish(msg)
+		rate.sleep()
+
+if __name__ == '__main__':
+	try:
+		publisher()
+	except rospy.ROSInterruptException():
+		pass
---
+++ ros_basics/scripts/msg_subscriber.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python 
+import rospy 
+from std_msgs.msg import String, Int32
+from ros_basics.msg import greet
+
